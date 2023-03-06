1821f241743d203eb967910805bad322d4728369
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-03-06 21:48:59 +0100

    Update Acceuil.py

--- streamlit/Acceuil.py
+++ streamlit/Acceuil.py
@@ -1,13 +1,13 @@
-# & C:/Users/laura/.env/ChallengeSecurite/Scripts/Activate.ps1
 import streamlit as st
 import pandas as pd
 
 def main():
     st.header("Application projet de Sécurité")
     st.write('SISE : BOUTONNET Laura, CORNU Christelle, HOUDE Titouan, SPICA Loïc')
-    st.write('OPSIE : DJEDDOU Djalal Eddine, HALFAOUI Abdellah')
 
 if __name__ == '__main__':
     main()
 
-st.write('Blabla Projet')
+st.write('Ce projet est réalisé dans le cadre des cours de données de sécurité des M2 SISE et OPSIE de l\'université Lyon 2 Lumière. Il nous à été demandé d\'effectuer des analyses des données d\'un équipement de sécurité et fournir des métriques propres au SI herbergé.')
+st.write('Ce projet à été réalisé en 27 heures, il aurait du être réalisé en collaboration entre deux étudiants d\'OPSIE et quatres étudiants de SISE')
+st.write('Cette application est un outil de datavizualisation permettant d\'avoir des informations sur les logs étudiés')
